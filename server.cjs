b52aa1ba2c010df90fa2dc2cc51afabc026c4e29
Wiggyboard <105466565+Wiggyboard@users.noreply.github.com>
2024-01-15 21:29:11 -0800

    Added https

--- server.cjs
+++ server.cjs
@@ -3,6 +3,17 @@ dotenv.config({ path: './config.env' });
 const app = require('./app.cjs');
 const mongoose = require('mongoose');
 const mongodb = process.env.DATABASE.replace('<password>', process.env.DB_PASSWORD);
+const https = require('https');
+const fs = require('fs');
+
+// SSL certificate
+const privateKey = fs.readFileSync('/etc/ssl/private/private.key', 'utf8');
+const certificate = fs.readFileSync('/etc/ssl/certs/certificate.crt', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate
+};
 
 mongoose.connect(mongodb, {
   useNewUrlParser: true,
@@ -13,6 +24,10 @@ mongoose.connect(mongodb, {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+
+// Creates an HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`App running on port ${port} over HTTPS...`);
 });
