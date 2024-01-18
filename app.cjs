11529024deff26114fcaf18174e3d0b6603ef264
Wiggyboard <105466565+Wiggyboard@users.noreply.github.com>
2024-01-17 17:15:01 -0800

    revert

--- app.cjs
+++ app.cjs
@@ -4,12 +4,7 @@ const userRouter = require('./userRoutes.cjs');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'https://wiggyboard.com',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/tunetracker`));
 
--- userRoutes.cjs
+++ userRoutes.cjs
@@ -1,9 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const userController = require('./userController.cjs');
 const router = express.Router();
-
-router.use(cors());
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
